b7e15db63d6b20caca379a359218a0f279b3eb4a
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-09-12 20:20:20 -0400

    add navigation

--- inqubate-website/src/app/Landing.jsx
+++ inqubate-website/src/app/Landing.jsx
@@ -6,13 +6,17 @@ export default function Landing(){
         <>
             <nav className="px-20 py-6 flex">
                 <Image src="inqubate.svg" alt="inqubate" width={250} height={250} className=""/>
-                <div className="grow flex gap-10 justify-center">
+                <div className="grow flex gap-5 justify-center">
                     <Navigation href="#home">Home</Navigation>
                     <Navigation href="#who-we-are">Who we are</Navigation>
                     <Navigation href="#ventures">Ventures</Navigation>
                     <Navigation href="#blog">Blog</Navigation>
                 </div>
-                <a href="#contact-us" className="">Contact us</a>
+                <a
+                href="#contact-us"
+                className="w-48 rounded-md text-center px-6 py-4 bg-gradient-to-r from-red-500 to-pink-700 shadow-[2px_5px_20px_grey] hover:-translate-y-1 transition-transform">
+                    Contact Us
+                </a>
             </nav>
             <Image
             src="tube.svg"
@@ -21,21 +25,21 @@ export default function Landing(){
             sizes="100vw"
             style={{ width: '100%', height: 'auto' }} 
             alt="tube"
-            className="absolute"
+            className="absolute -z-10"
             />
             <Image
             src="red-gradient.svg"
             width={900}
             height={700}
             alt="red gradient"
-            className="absolute -z-10 top-0"
+            className="absolute -z-50 top-0"
             />
             <Image
             src="blue-gradient.svg"
             width={1000}
             height={700}
             alt="blue gradient"
-            className="absolute -z-10 right-0"
+            className="absolute -z-50 right-0"
             />
         </>
     )
--- inqubate-website/src/app/Navigation.jsx
+++ inqubate-website/src/app/Navigation.jsx
@@ -1,5 +1,7 @@
 export default function Navigation({children, href}){
     return (
-        <a href={href} className="hover:border-2 hover:rounded-md py-2 px-4 hover:font-bold">{children}</a>
+        <a href={href} className="hover:border-2 hover:rounded-md px-4 hover:font-bold items-center  justify-center flex w-32 transition-[font-weight] transition-transform hover:-translate-y-1">
+            <span>{children}</span>
+        </a>
     )
 }
