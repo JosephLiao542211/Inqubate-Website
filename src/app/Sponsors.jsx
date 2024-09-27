a42b1c2d90da22d672a20f45d7a90395887d6e64
KungPaoMeow <johnli8642@gmail.com>
2024-09-27 14:02:55 -0400

    replace rbc image with not cut off version

--- src/app/Sponsors.jsx
+++ src/app/Sponsors.jsx
@@ -4,7 +4,7 @@ export default function Sponsors(){
             <img src="mckinsey.png" alt="mckinsey" className="w-96"/>
             <img src="wealthsimple.png" alt="wealthsimple" className="w-96"/>
             <img src="aws.png" alt="aws" className="w-96"/>
-            <img src="rbc.png" alt="rbc" className="w-96 rounded-3xl"/>     {/* This is rounded to make it look better but logo is off-centered! */}
+            <img src="rbc.png" alt="rbc" className="w-96"/>     {/* This is rounded to make it look better but logo is off-centered! */}
         </>
     )
 }
