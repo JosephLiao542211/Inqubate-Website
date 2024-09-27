4e208a4811bae22912261f3d061d1af8032dad88
John Li <johnli8642@gmail.com>
2024-09-27 10:44:21 -0400

    make sponsor sliding smooth with useEffect

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     extend: {
       keyframes: {
         slide: {
-          "0%": {transform: "translateX(-100%)"},
+          "0%": {transform: "translateX(0%)"},
           "100%": {transform: "translateX(0)"}
         },
         fade: {
