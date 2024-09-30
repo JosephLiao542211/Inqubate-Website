3ecc969bf6d8bd28bdb1650f3fffcc6e92015be0
Kevin Ye <156462786+kzy0525@users.noreply.github.com>
2024-09-30 14:47:03 -0400

    Create teamMembers.js

---
+++ teamMembers.js
@@ -0,0 +1,39 @@
+const teamMembers = [
+    {
+      id: 1,
+      name: 'Daniel Wang',
+      title: 'Co-Chair',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare vel urna vitae venenatis. Cras sit amet erat dapibus, sodales lacus eget, lacinia mauris. Curabitur sed ex eu ligula vehicula vestibulum. Vivamus nec fringilla mauris, at condimentum erat. Aliquam orci lacus, maximus ut mollis nec, tempus sed erat. Vestibulum dapibus mauris vitae elit cursus, a tempor ligula feugiat.',
+      photolg: '/images/daniel.jpg',
+      photosm: '/images/josephliao.png',
+      
+    },
+    {
+      id: 2,
+      name: 'Joseph Liao',
+      title: 'Technology Consultant',
+      bio: 'Curabitur tincidunt consequat velit eu vestibulum. Fusce ligula augue, semper eu mauris ultricies, vestibulum sagittis dolor. Cras aliquam velit nec suscipit maximus. Nulla rhoncus vel purus vel vulputate. Quisque hendrerit arcu nisl, eget tempus magna molestie non. Nunc id lectus nec sapien feugiat auctor at vel libero. Etiam condimentum fermentum urna id posuere. Vestibulum aliquam lacus elit.',
+      photolg: '/images/joseph.png',
+      photosm: '/images/josephliao.png',
+    },
+    {
+      id: 3,
+      name: 'Jacob Kranjac', 
+      title: 'Co-Chair', 
+      bio: 'Pellentesque massa eros, suscipit eu neque eu, viverra rhoncus lectus. Nam feugiat, ligula non interdum venenatis, sapien metus cursus enim, eu volutpat lectus justo id augue. Pellentesque lobortis velit non odio placerat aliquam. Praesent ut lorem in metus euismod interdum. Vestibulum a accumsan tellus, in placerat libero. In leo tortor, feugiat in felis placerat, maximus efficitur ex.',
+      photolg: '/images/jacob.png',
+      photosm: '/images/josephliao.png',
+    }, 
+    {
+      id: 4,
+      name: 'Evan Ferreira',
+      title: 'Co-Chair',
+      bio: 'Suspendisse sed leo non eros laoreet lobortis vel gravida leo. Pellentesque eget ex mattis, volutpat eros non, egestas metus. Mauris vitae nisl eget sapien vulputate porttitor. Duis tempus quam pellentesque dignissim lacinia. Vivamus libero nisi, scelerisque at justo sit amet, fringilla gravida urna.',
+      photolg: '/images/evan.jpg',
+      photosm: '/images/josephliao.png',
+    }
+    // Add more members here
+  ];
+  
+  export default teamMembers;
+  
