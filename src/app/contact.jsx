6e5fa394979135f353142b6ab535bdbe23a1d615
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-10-22 10:19:58 -0400

    slight change to contact

--- src/app/contact.jsx
+++ src/app/contact.jsx
@@ -1,27 +1,23 @@
 "use client";
-import Contactform from './contactform.jsx';
-import Image from 'next/image.js';
-import React, {  } from 'react';
+import Contactform from "./contactform.jsx";
+import Image from "next/image.js";
+import React from "react";
 
 const Contact = () => {
   return (
-    <div className='absolute'>
-        <img
-        src="/loop.svg"
-        alt="loop"
-        className="absolute right-0 -bottom-[28rem]"
-        />
-
-      <div className='ml-64 w-1/2'>
+    <div className="absolute">
+      <div className="ml-64 w-1/2">
         <h1 className="text-6xl font-bold mb-4">Support Us</h1>
         <p className="mb-8">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae inventore asperiores ducimus, ipsam cupiditate quisquam optio officia tenetur culpa rurur ugte peokekrmr hgrbh jeje jee.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
+          inventore asperiores ducimus, ipsam cupiditate quisquam optio officia
+          tenetur culpa rurur ugte peokekrmr hgrbh jeje jee.
         </p>
         <Contactform />
       </div>
-
     </div>
   );
 };
 
 export default Contact;
+
