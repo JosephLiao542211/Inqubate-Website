10a8e862252ca59c0939d4d2df732162559504bc
Evan Ferreira <evanjfer@gmail.com>
2024-10-25 18:37:15 -0400

    Added Text to All Sections

--- src/app/contact.jsx
+++ src/app/contact.jsx
@@ -1,23 +1,24 @@
-"use client";
-import Contactform from "./contactform.jsx";
-import Image from "next/image.js";
-import React from "react";
+'use client';
+import Contactform from './contactform.jsx';
+import Image from 'next/image.js';
+import React from 'react';
 
 const Contact = () => {
-  return (
-    <div className="absolute">
-      <div className="ml-64 w-1/2">
-        <h1 className="text-6xl font-bold mb-4" id="contact">Support Us</h1>
-        <p className="mb-8">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
-          inventore asperiores ducimus, ipsam cupiditate quisquam optio officia
-          tenetur culpa rurur ugte peokekrmr hgrbh jeje jee.
-        </p>
-        <Contactform />
-      </div>
-    </div>
-  );
+    return (
+        <div className="absolute">
+            <div className="ml-64 w-1/2">
+                <h1 className="text-6xl font-bold mb-4" id="contact">
+                    Support Us
+                </h1>
+                <p className="mb-8">
+                    Interested in supporting inQUbate or becoming a member? Fill
+                    out the form below and we will get back to you as soon as
+                    possible.
+                </p>
+                <Contactform />
+            </div>
+        </div>
+    );
 };
 
 export default Contact;
-
