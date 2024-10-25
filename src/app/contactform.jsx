1b4155a4ccdaede2bf29b4950caaad9c4553ca44
Gavin Yan <gavinyan10@gmail.com>
2024-10-25 19:22:16 -0400

    re-implement send button

--- src/app/contactform.jsx
+++ src/app/contactform.jsx
@@ -6,11 +6,13 @@ import React, { useRef, useState } from 'react';
 export const ContactUs = () => {
 
   const [isSubmitted, setIsSubmitted] = useState(false); // Change state to track submission
+  const [sending, setSending] = useState(false); 
   const form = useRef();
 
 
   const sendEmail = (e) => {
     e.preventDefault();
+    setSending(true);
 
     emailjs
       .sendForm('service_0tuvwuv', 'template_gxpi41h', form.current, {
@@ -20,6 +22,7 @@ export const ContactUs = () => {
         () => {
           console.log('SUCCESS!');
           setIsSubmitted(true);
+          setSending(false);
         },
         (error) => {
           console.log('FAILED...', error.text);
@@ -35,6 +38,7 @@ export const ContactUs = () => {
       name="user_name"
       className="border border-gray-900 p-2 rounded-[10px] bg-gray-900 text-white placeholder-gray-400 w-[747px] h-10" // Customize size
       placeholder="Enter your name"
+      required
     />
     
     
@@ -43,20 +47,28 @@ export const ContactUs = () => {
       name="user_email"
       className="border border-gray-900 p-2 rounded-[10px] bg-gray-900 text-white placeholder-gray-400 w-[747px] h-12" // Customize size
       placeholder="Enter your email"
+      required
     />
     
     <textarea
       name="message"
       className="border border-gray-900 p-2 rounded-[10px] bg-gray-900 text-white placeholder-gray-400 w-[747px] h-[150px] resize-none" // Customize size
       placeholder="Enter your message"
+      required
     />
-    
+    {!isSubmitted && !sending && (
       <button
           className="font-bold border border-white pb-1 rounded-lg cursor-pointer w-44 h-12 transition-transform duration-400 hover:-translate-y-1 hover:bg-slate-900 mt-8"
       >
-        {isSubmitted ? "Completed" : "Submit"}
+        Submit
       </button>
-
+    )}
+    {sending && (
+      <p className='h-14'>Sending...</p>
+    )}
+    {isSubmitted && (
+      <p className='h-14'>Thanks for contacting us!</p>
+    )}
     </form>
     
 
